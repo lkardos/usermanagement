25dd32b5d7fe973e8cfa67560e34202b434a3b98
Lubos Kardos <kardos.lubos@gmail.com>
2024-02-01 19:18:58 +0100

    Enable CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.UserManagement import User, UserRepository
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 
 class UserInfo(BaseModel):
     username: str
@@ -11,6 +11,21 @@ class UserInfo(BaseModel):
 
 user_repository = UserRepository()
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
