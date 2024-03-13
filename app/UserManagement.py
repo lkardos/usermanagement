ce435cf62ab5db8a9c4cadf0e4c5e3fc26265c1d
Lubos Kardos <kardos.lubos@gmail.com>
2024-03-13 23:39:26 +0100

    Add functionality * login * logout * validate authorization token

--- app/UserManagement.py
+++ app/UserManagement.py
@@ -1,14 +1,34 @@
+import random
+import string
+
+
 class User:
     def __init__(self, username=None, email=None, password=None, user=None):
         if user is not None:
-            self.username = user.username
-            self.email = user.email
-            self._password_hash = user._password_hash
+            self._copy_from(user)
         else:
             # TODO: Validate fields
             self.username = username
             self.email = email
             self._password_hash = hash_password(password)
+            self._token = None
+
+    def _copy_from(self, user):
+        self.username = user.username
+        self.email = user.email
+        self._password_hash = user._password_hash
+        self._token = user._token
+
+    def login(self, password):
+        if self._password_hash != hash_password(password):
+            raise Unauthorized
+
+        self._token = generate_token()
+        return self._token
+
+    def logout(self, token):
+        if self._token == token:
+            self._token = None
 
 
 class UserRepository:
@@ -18,8 +38,18 @@ class UserRepository:
     def add(self, user):
         self.users.append(user)  # TODO: Check uniqueness of the username
 
+    def update_user(self, user):
+        key = next(key for key, current_user in enumerate(self.users) if current_user.username == user.username)
+        self.users[key] = User(user=user)
+
     def get_user(self, username):
         return User(user=next(user for user in self.users if user.username == username))  # TODO: Throw better exception than StopIteration
+
+    def get_user_by_token(self, token):
+        user = next((user for user in self.users if user._token == token), None)
+        if user is None:
+            raise UserNotFound("Invalid token")
+        return User(user=user)
 
     def list_users(self):
         return [User(user=user) for user in self.users]
@@ -27,3 +57,15 @@ class UserRepository:
 
 def hash_password(password):
     return password  # TODO: Do some real hashing
+
+
+def generate_token():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=256))
+
+
+class Unauthorized(Exception):
+    pass
+
+
+class UserNotFound(Exception):
+    pass
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
-from app.UserManagement import User, UserRepository
+from fastapi import FastAPI, HTTPException
+from app.UserManagement import User, UserRepository, UserNotFound
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+
 
 class UserInfo(BaseModel):
     username: str
@@ -10,13 +11,13 @@ class UserInfo(BaseModel):
 
 
 user_repository = UserRepository()
+user_repository.add(User("test_user", "test_user@test.com", "test_password"))
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:8080",
-    "http://localhost:3000",
+    "http://alphahr",
+    "http://alphahr:8080",
+    "http://alphahr:3000",
 ]
 
 app.add_middleware(
@@ -39,5 +40,26 @@ def add_user(user_info: UserInfo):
     user_repository.add(User(user_info.username, user_info.email, user_info.password))
 
 
+@app.post("/login")
+def login(username: str, password: str):
+    user = user_repository.get_user(username)
+    token = user.login(password)
+    user_repository.update_user(user)
+    return token
 
+
+@app.post("/logout")
+def logout(username: str, token: str):
+    user = user_repository.get_user(username)
+    user.logout(token)
+    user_repository.update_user(user)
+
+
+@app.post("/validate_token")
+def validate_token(token: str):
+    try:
+        user = user_repository.get_user_by_token(token)
+        return user
+    except UserNotFound:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
